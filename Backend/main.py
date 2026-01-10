6ca2c3f8be186f28138cfa5ecf90c33913f3f900
leducvu192005 <vule59695@gmail.com>
2026-01-10 14:49:11 +0700

    sua them do an

--- Backend/main.py
+++ Backend/main.py
@@ -4,11 +4,13 @@ from models import Base
 from routers.auth_router import router as auth_router
 from routers.coupons_router import router as coupons_router
 from routers.products_router import router as products_router
-from category import  router as category_router
+from routers.category import  router as category_router
+from routers.dish import router as dish_router
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Food Delivery App")
 app.include_router(category_router)
+app.include_router(dish_router)
 app.include_router(auth_router)
 app.include_router(products_router)
 app.include_router(coupons_router)
