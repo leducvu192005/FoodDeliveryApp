57a5bb8643ef6aac0c7cdefd55a75867b55fbff3
leducvu192005 <vule59695@gmail.com>
2026-01-10 14:46:13 +0700

    sua loi them danh muc

--- Backend/database.py
+++ Backend/database.py
@@ -3,21 +3,23 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# Load biến từ .env
 load_dotenv()
 
-# Lấy thông tin từ environment
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_NAME = os.getenv("DB_NAME", "Food_Delivery_App")
-DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_PORT = os.getenv("DB_PORT", "5432")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASE_URL = os.getenv("postgresql://postgres.pwwkqdizdbxvgpbysfgy:5*d?z_B?Vh#qdsF@aws-1-ap-southeast-1.pooler.supabase.com:5432/postgres")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set. Check your .env file")
 
-engine = create_engine(DATABASE_URL,pool_pre_ping=True)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 def get_db():
     db = SessionLocal()
